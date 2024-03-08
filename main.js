b7e52390e3af251c60a177f829904caf921ba9fb
Asfa Qasim <160766420+AsfaQasim@users.noreply.github.com>
2024-03-08 08:36:08 -0800

    Q 25 of Ts/js & node js of 45 assignment.
    Q 25 of Ts/js & node js of 45 assignment.

---
+++ main.js
@@ -0,0 +1,9 @@
+//create a  variable called alien_color
+var alien_color = "green";
+if (alien_color == "green") {
+    console.log("the player just earned 5 points.");
+}
+alien_color = "red";
+if (alien_color == "green") {
+    console.log("the player just earned 5 points.");
+}
---
+++ main.ts
@@ -0,0 +1,12 @@
+//create a  variable called alien_color
+let alien_color: string  ="green";
+if(alien_color == "green"){
+    console.log("the player just earned 5 points.");
+
+}
+alien_color = "red";
+if(alien_color == "green"){
+    console.log("the player just earned 5 points.");
+
+}
+
